5a6d891f8ddecf68fb3608cb10947cb0318472f0
mike.glamm <mike.glamm@gmail.com>
2018-01-24 16:45:29 -0500

    Better mouse event response

--- js/piano.js
+++ js/piano.js
@@ -2,32 +2,48 @@
 
 var audioContext = new AudioContext();
 var osc;
+var mouseDownFlag = false;
 
-function playkey(key)
-{
+function playkey(key) {
   osc = audioContext.createOscillator();
   osc.type = "sine";
-  osc.frequency.value = Math.pow(2, (key-49)/12)*440;
+  osc.frequency.value = Math.pow(2, (key - 49) / 12) * 440;
   osc.connect(audioContext.destination);
   osc.start(audioContext.currentTime);
 }
 
-function stopkey()
-{
-  osc.stop(audioContext.currentTime);
+function stopkey() {
+  if (osc) {
+    osc.stop(audioContext.currentTime);
+  }
 }
 
 $(function() {
   $(".key").on("mousedown", function() {
-    var keyid = $(this).attr("id");
-    playkey(keyid);
-  });
-  $(".key").on("touchstart", function() {
+    mouseDownFlag = true;
     var keyid = $(this).attr("id");
     playkey(keyid);
   });
   $(".key").on("mouseup", function() {
+    mouseDownFlag = false;
     stopkey();
+  });
+  $(".key").on("mouseenter", function() {
+    if (mouseDownFlag) {
+      stopkey();
+      var keyid = $(this).attr("id");
+      playkey(keyid);
+    }
+  });
+  $(".key").on("mouseout", function() {
+    stopkey();
+  });
+  $(window).mouseup(function() {
+    mouseDownFlag = false;
+  });
+  $(".key").on("touchstart", function() {
+    var keyid = $(this).attr("id");
+    playkey(keyid);
   });
   $(".key").on("touchend", function() {
     stopkey();
