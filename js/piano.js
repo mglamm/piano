8632cb29064103129b6b1db67bfe073ebbab3cd6
mike.glamm <mike.glamm@gmail.com>
2018-01-26 12:44:53 -0500

    Add support for multi-touch

--- js/piano.js
+++ js/piano.js
@@ -3,19 +3,49 @@
 var audioContext = new AudioContext();
 var osc;
 var mouseDownFlag = false;
+var touchKeysPlaying = new Map();
+
+function setupOscillator(key) {
+  var localOsc = audioContext.createOscillator();
+  localOsc.type = "sine";
+  localOsc.frequency.value = Math.pow(2, (key - 49) / 12) * 440;
+  localOsc.connect(audioContext.destination);
+  localOsc.start(audioContext.currentTime);
+  return localOsc;
+}
 
 function playkey(key) {
-  osc = audioContext.createOscillator();
-  osc.type = "sine";
-  osc.frequency.value = Math.pow(2, (key - 49) / 12) * 440;
-  osc.connect(audioContext.destination);
-  osc.start(audioContext.currentTime);
+  osc = setupOscillator(key);
 }
 
 function stopkey() {
   if (osc) {
     osc.stop(audioContext.currentTime);
   }
+}
+
+function reconcileTouches(currentTouches) {
+  // stop playing any keys not currently pressed
+  touchKeysPlaying.forEach(function(value, key) {
+    var found = false;
+    currentTouches.forEach(function(item, index, array) {
+      if(Number(item.target.id) == key) {
+        found = true;
+      }
+    });
+    if(!found) {
+      value.stop(audioContext.currentTime);
+      touchKeysPlaying.delete(key);
+    }
+  });
+  // start playing new pressed keys
+  currentTouches.forEach(function(item, index, array) {
+    var keyid = Number(item.target.id);
+    if(!touchKeysPlaying.has(keyid)) {
+      var newOsc = setupOscillator(keyid);
+      touchKeysPlaying.set(keyid, newOsc);
+    }
+  });
 }
 
 $(function() {
@@ -43,11 +73,14 @@ $(function() {
   });
   $(".key").on("touchstart", function(e) {
     e.preventDefault();
-    var keyid = $(this).attr("id");
-    playkey(keyid);
+    reconcileTouches(e.touches);
   });
+  $(".key").on("touchmove", function(e) {
+    e.preventDefault();
+    reconcileTouches(e.touches);
+  })
   $(".key").on("touchend", function(e) {
     e.preventDefault();
-    stopkey();
+    reconcileTouches(e.touches);
   });
 });
